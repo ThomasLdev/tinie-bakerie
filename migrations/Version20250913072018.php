147a9cbda64e88104378363999ad0f560124e601
thomas-lefebvre <t.lefebvre.div@gmail.com>
2025-09-13 09:20:51 +0200

    - fix schema diff

---
+++ migrations/Version20250913072018.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250913072018 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_media_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE category_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE post_media_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE post_section_media_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE post_section_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE post_translation ALTER translatable_id SET NOT NULL');
+        $this->addSql('ALTER TABLE tag_translation ALTER translatable_id SET NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE post_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE category_media_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE tag_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE category_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE post_section_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE post_section_media_translation ALTER translatable_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE post_media_translation ALTER translatable_id DROP NOT NULL');
+    }
+}
