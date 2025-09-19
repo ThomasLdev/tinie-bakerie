534af8b9d61e4f4bbf3f3a6b9045aa8e1c9b5ea0
thomas <t.lefebvre.div@gmail.com>
2025-09-20 00:59:34 +0200

    - remove mercure, test prod mode with workers,add tailwind build in e… (#14)
    * - remove mercure, test prod mode with workers,add tailwind build in entrypoint
    
    * - remove flowbite, fix tailwind 4 setup, use make for db CI setup, upgrade stimulus
    
    * - fix CI step
    
    * - fix CI step

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,3 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-document.addEventListener('turbo:render', () => {
-  initFlowbite();
-});
-document.addEventListener('turbo:frame-render', () => {
-  initFlowbite();
-});
